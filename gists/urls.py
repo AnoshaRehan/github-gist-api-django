5618b80a1589b86e13870c67163bc77b7e2d1a85
AnoshaRehan <anosha.rehan@lovefordata.com>
2021-02-13 23:15:55 +0500

    added forks

--- gists/urls.py
+++ gists/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.urls import path
-from gists.views import GetPublicGistAPI, GetUserGistsAPI
+from gists.views import GetPublicGistAPI, GetUserGistsAPI, GetForksAPI
 
 app_name = "gists"
 
 urlpatterns = [
     path('getGists/', GetPublicGistAPI.as_view()),
-    url(r'^getUserGist/$', GetUserGistsAPI.as_view())
+    url(r'^getUserGist/$', GetUserGistsAPI.as_view()),
+    url(r'^getForks/$', GetForksAPI.as_view())
 ]
--- gists/views.py
+++ gists/views.py
@@ -24,6 +24,20 @@ class GetPublicGistAPI(APIView):
 class GetUserGistsAPI(APIView):
 
     def get(self, request):
-        url = self.GITHUB_API + "/users/{}/gists".format(request.GET['username'])
+        url = GITHUB_API + "/users/{}/gists".format(request.GET['username'])
         res = requests.get(url)
         return Response(res, status=200)
+
+class GetForksAPI(APIView):
+
+    def get(self, request):
+        url = GITHUB_API + "/gists/{}/forks".format(request.GET['gist_id'])
+        res = requests.get(url)
+        user_avatar = {}
+        data = json.loads(res.text)
+        for i in range(len(data)):
+            avatar = data[i]['owner']['avatar_url']
+            username = data[i]['owner']['login']
+            if username not in user_avatar:
+                user_avatar[username] = avatar
+        return Response(user_avatar, status=200)
